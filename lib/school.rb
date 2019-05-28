62bc4bf87491f6cabe1f146bba657c19e759f4db
Peter <cirediatpl@gmail.com>
2019-05-28 14:20:07 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+    attr_reader :roster
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+
+    def add_student(student_name, grade)
+        if @roster[grade] != nil
+            @roster[grade] << student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster.values_at(grade).flatten
+    end
+
+    def sort
+        hash = {}
+         @roster.each do |key, var|
+            @roster[key] = var.sort
+        # @roster.map do |key, var|
+        #     @roster[key] = var.sort
+        # why does this create an array
+         end
+    end
+end
